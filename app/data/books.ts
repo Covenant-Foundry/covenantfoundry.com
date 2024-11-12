1b0dfd44cf502e4721c36e514e1924cc00adfbf4
Jon Winsley <jwinsley@jbssolutions.com>
2024-11-11 21:06:37 -0500

    adding images locally

---
+++ app/data/books.ts
@@ -0,0 +1,24 @@
+import ventureDeals from "~/assets/images/venture-deals.jpg";
+import zeroToOne from "~/assets/images/zero-to-one.jpg";
+
+export const inspiration = [
+  {
+    title: "Zero to One",
+    description: "Notes on Startups, or How to Build the Future",
+    category: "Inspiration",
+    tags: ["Entrepreneurship", "Inspiration"],
+    link: "https://www.amazon.com/Zero-One-Notes-Startups-Future/dp/0804139296/",
+    imageUrl: zeroToOne,
+  },
+];
+
+export const business = [
+  {
+    title: "Venture Deals",
+    description: "A handbook of agile software craftsmanship",
+    category: "Programming",
+    tags: ["Software", "Best Practices"],
+    link: "https://example.com",
+    imageUrl: ventureDeals,
+  },
+];
