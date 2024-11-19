2259f657fde26ae2c46d0af160aa68eff256c1c4
Jon Winsley <jon.winsley@gmail.com>
2024-11-18 20:42:09 -0500

    setting up turso

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,10 +1,21 @@
+import { invariant } from '@epic-web/invariant'
 import { remember } from '@epic-web/remember'
+import { createClient } from '@libsql/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 import chalk from 'chalk'
 
 export const prisma = remember('prisma', () => {
 	// NOTE: if you change anything in this function you'll need to restart
 	// the dev server to see your changes.
+
+	invariant(process.env.TURSO_DATABASE_URL, 'TURSO_DATABASE_URL not provided')
+	invariant(process.env.TURSO_AUTH_TOKEN, 'TURSO_AUTH_TOKEN not provided')
+	const libsql = createClient({
+		url: process.env.TURSO_DATABASE_URL,
+		authToken: process.env.TURSO_AUTH_TOKEN,
+	})
+	const adapter = new PrismaLibSQL(libsql)
 
 	// Feel free to change this log threshold to something that makes sense for you
 	const logThreshold = 20
@@ -15,6 +26,7 @@ export const prisma = remember('prisma', () => {
 			{ level: 'error', emit: 'stdout' },
 			{ level: 'warn', emit: 'stdout' },
 		],
+		adapter,
 	})
 	client.$on('query', async (e) => {
 		if (e.duration < logThreshold) return
